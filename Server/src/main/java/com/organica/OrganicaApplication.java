a9ad61b0e59f1aa0ea5f79486f9226452303572b
bob <b@tcs.com>
2025-11-11 16:11:49 +0530

    modified CORS

--- Server/src/main/java/com/organica/OrganicaApplication.java
+++ Server/src/main/java/com/organica/OrganicaApplication.java
@@ -27,7 +27,10 @@ public class OrganicaApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
 			}
 		};
 	}
